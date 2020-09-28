5c4d0beab55d1c08b1032f97138f30c7e5071086
antcha19 <yepez261@gmail.com>
2020-09-28 13:54:48 +0200

    ejercicios

---
+++ Combo/src/combo/combos.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package combo;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+import java.awt.event.ItemListener;
+
+public class combos extends JFrame {
+    
+    private  JComboBox combo;
+    private JPanel panel;
+    private JLabel label;
+    private String[] images={"Rabbit", "Cat", "Dog", "Bird", "Pig"};
+    private Icon img;
+    
+    public combos(){
+        setSize(400, 500);
+        setTitle("combos and images");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+       
+        
+        combo=new JComboBox(images);
+        panel.add(combo,BorderLayout.NORTH);
+        combo.addItemListener(new Listenercombo());
+        
+        System.out.println(System.getProperty("java.class.path"));
+ 
+        img = new ImageIcon (getClass().getResource("images/Rabbit.gif"));
+        label = new JLabel();
+        label.setText("Rabbit");
+        label.setIcon(img);
+        
+      
+        label.setHorizontalTextPosition(SwingConstants.CENTER);
+        label.setVerticalTextPosition(SwingConstants.BOTTOM);
+        label.setToolTipText("esto es um label");
+        panel.add(label,BorderLayout.CENTER);
+        
+        add(panel);
+        
+             
+    }
+    private class Listenercombo implements ItemListener{
+        public void itemStateChanged(ItemEvent e){
+            int indice=combo.getSelectedIndex();
+            String s = images[indice];
+            img = new ImageIcon(getClass().getResource("images/" + s + ".gif"));
+            label.setIcon(img);
+            label.setText(s);
+        }
+    }
+}
--- Ejerciciohijos/src/ejerciciohijos/frasehijos.java
+++ Ejerciciohijos/src/ejerciciohijos/frasehijos.java
@@ -11,23 +11,25 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import javax.swing.JCheckBox;
 
-
 public class frasehijos extends JFrame {
 
     private JTextField textField;
     private JCheckBox daugthersCheckBox;
     private JCheckBox sonsCheckBox;
     private JCheckBox noneCheckBox;
+    private JLabel label;
 
     //radioBotton
     // grupo de botones que contiene los botones de opción
     private ButtonGroup optionsGroup;
     private JRadioButton maleJRadioButton;
     private JRadioButton femaleJRadioButton;
+
     public frasehijos() {
 
         setTitle("frase");
         setLayout(new FlowLayout());
+        label = new JLabel();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         daugthersCheckBox = new JCheckBox("Daugthers");
@@ -36,33 +38,60 @@ public class frasehijos extends JFrame {
         add(daugthersCheckBox);
         add(sonsCheckBox);
         add(noneCheckBox);
-       ListenerCheckBox manejador1 = new ListenerCheckBox();
-       daugthersCheckBox.addItemListener(manejador1);
-       sonsCheckBox.addItemListener(manejador1);
-       noneCheckBox.addItemListener(manejador1);
-        
+        ListenerCheckBox manejador1 = new ListenerCheckBox();
+        daugthersCheckBox.addItemListener(manejador1);
+        sonsCheckBox.addItemListener(manejador1);
+        noneCheckBox.addItemListener(manejador1);
+
         //creo los botons 
-        maleJRadioButton = new JRadioButton("Male");
-        femaleJRadioButton = new JRadioButton("Female");
+        maleJRadioButton = new JRadioButton("Male", true);
+        femaleJRadioButton = new JRadioButton("Female", false);
         add(maleJRadioButton);
         add(femaleJRadioButton);
-      //  maleJRadioButton.addItemListener(new );
-        
-        
+        maleJRadioButton.addItemListener(new ListenerCheckBox());
+
         //creo una relacion logica entre los objetos RadioButton
         optionsGroup = new ButtonGroup();
         optionsGroup.add(maleJRadioButton);
         optionsGroup.add(femaleJRadioButton);
-        
-        //creo el textfield
-        textField = new JTextField(20);
-        add(textField); 
+
+        add(label);
 
     }
-    private class ListenerCheckBox implements ItemListener{
-        public void itemStateChanged(ItemEvent event){
-            
+
+    private class ListenerCheckBox implements ItemListener {
+
+        private boolean dauthers;
+        private String dau;
+        private String sons;
+        private String none;
+        private String male;
+        private String dos;
+        private String female;
+
+        
+        public void itemStateChanged(ItemEvent event) {
+            if (event.getSource() == daugthersCheckBox) {
+                dau = "tengo hijas";
+                label.setText(dau);
+            }
+            if (event.getSource() == sonsCheckBox) {
+                sons = "you are sons";
+                label.setText(sons);
+            } else if (event.getSource() == sonsCheckBox && event.getSource() == daugthersCheckBox) {
+                dos = "You are sons and daugthers";
+                label.setText(dos);
+            } else if (event.getSource() == noneCheckBox) {
+                none = "no tengo hijos";
+                label.setText(none);
+            }
+            /*if(maleJRadioButton.isSelected()==true){
+                male = "and you are male";
+                label.setText(male);
+            }*/
+
         }
+
     }
 
 }
