05355099fd67f504c37bc0f5fd68ec7b29a2cf5a
antcha19 <yepez261@gmail.com>
2020-10-27 19:07:40 +0100

    ejercicios

---
+++ DbudgetPC/src/budgetpc/BudgetPC.java
@@ -0,0 +1,13 @@
+
+
+package budgetpc;
+
+
+public class BudgetPC {
+    
+    public static void main(String[] args) {
+         frame f = new frame();
+         f.setVisible(true);
+    }
+    
+}
