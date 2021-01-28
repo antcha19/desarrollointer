3943202d27de111a1cff7640573fd9df03c15196
antcha19 <yepez261@gmail.com>
2021-01-28 08:13:06 +0100

    psp

---
+++ PSP/DATA/src/data/EnvioMensaje.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author antcha
+ */
+public class EnvioMensaje {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws UnknownHostException, SocketException, IOException {
+        //bytes del mensaje a enviar
+        Scanner teclado = new Scanner(System.in);
+        //variables
+        String mensajeintroducido = "";
+           System.out.println("Introduce el mensaje a enviar");
+        mensajeintroducido = teclado.nextLine();
+        
+        byte[] mensaje = mensajeintroducido.getBytes();
+
+        //ip de destino
+        InetAddress ipdestino = InetAddress.getByName("localhost");
+      //  InetAddress ipdestino = InetAddress.getByName("192.168.26.110");
+        //puerto destino
+        int portdestino = 5555;
+        try {
+            //creacion del paquete a enviar
+            DatagramPacket packet = new DatagramPacket(mensaje, mensaje.length, ipdestino, portdestino);
+            //creacion de un socket temporal con el que voy a realixar el envio
+            DatagramSocket socket = new DatagramSocket();
+            //enviar mensaje
+            socket.send(packet);
+         } catch (UnknownHostException ex) {
+            System.out.println("error ex");
+        }catch(SocketException soc){
+             System.out.println("error del socket");
+        }catch(IOException io){
+            System.out.println("error del io");
+        }
+       // String str1 = new String(mensaje);
+      //  System.out.println("mensaje enviado desde servidor "+str1);
+
+
+    }
+
+}
---
+++ PSP/DATA/src/data/RecepcionMensaje.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author antcha
+ */
+public class RecepcionMensaje {
+
+    public static void main(String[] args) throws UnknownHostException, SocketException, IOException {
+        //puerto que escucha el servicio
+        try {
+            int puertoescucha = 5555;
+        //vector de bytes en el cual recibir el mensaje con capacidad de 1.024 bytes
+            byte[] mensaje = new byte[1024];
+        //creacion del paquete en el qual rebre les dades de 1.024 bytes 
+            DatagramPacket packet = new DatagramPacket(mensaje, mensaje.length);
+       //creació d’un socket que escolti el port passat per paràmetre
+            DatagramSocket socket = new DatagramSocket(puertoescucha);
+       //recibe el paquete
+            socket.receive(packet);
+            
+            //mensaje traducido
+              String traducido = new String(packet.getData(), 0, packet.getLength());
+              System.out.println("Mensaje recibido  "+traducido);
+        } catch (UnknownHostException ex) {
+            System.out.println("error ex");
+        }catch(SocketException soc){
+             System.out.println("error del socket");
+        }catch(IOException io){
+            System.out.println("error del io");
+        }
+
+    }
+
+}
